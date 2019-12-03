3e7fc388417442f00eea9beb3b4f7fe36cd310f5
mustard123 <silasdavidweber@gmail.com>
2019-12-03 22:37:55 +0100

    dockerize

---
+++ docker/BBU/LoRa_Responder/lora_socket_server.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python2
+
+from __future__ import print_function
+import argparse
+import time
+import zmq
+import socket
+import os 
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+
+parser = argparse.ArgumentParser(description='Connect to udp port for receiving decoded LoRa signals,'
+' if an ACK is required publish ACK iq samples via zmq socket for Remote Radio Head to receive.')
+
+parser.add_argument('-p', '--port', default=5052, help='zmq port to publish downstream (i.e ACK) iq samples')
+args = parser.parse_args()
+
+context = zmq.Context()
+zmq_socket = context.socket(zmq.PUB)
+port = args.port
+zmq_socket.bind("tcp://*:%s" % port)
+print ('zmq publishes on all interfaces on port ' + str(port))
+
+
+with open (dir_path + "/gateway_downlink_trimmed_23_bytes_SF12_CR4.raw") as f:
+    ack = f.read()
+
+upd_port = 40868
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.bind(("127.0.0.1", upd_port))
+print ('listen for decoded lora packages on udp port ' + str(upd_port))
+while True:
+    data, addr = s.recvfrom(1024)
+
+    print ("decoded data is: ", data)
+    print ("as hex: ", data.encode("hex"))
+
+    # TODO Better to check if ACK is last 3 bytes of actual payload, attention lora_receiver also passes header and crc to us
+    if ("ACK" in data):
+        print ("received package request ACK, send ACK to remote radio head")
+        #TODO Acutally genearate appropriate down packet instead of sending recorded packet
+        #zmq_socket.send("hii") does not work with gnuradio
+        zmq_socket.send(ack) # works! gnuradio probably only works with passing complex types
+        print ('Done')
+    else:
+        print ("received package requests no ACK")
--- docker/base_image/run_image.sh
+++ docker/base_image/run_image.sh
@@ -9,4 +9,4 @@ DOCKER_XAUTH=/tmp/.docker.xauth
 touch /tmp/.docker.xauth
 xauth nlist $DISPLAY | sed -e 's/^..../ffff/' | xauth -f $DOCKER_XAUTH nmerge -
 
-docker run --net=host -i -t --rm --privileged -e DISPLAY=$DISPLAY -e XAUTHORITY=$DOCKER_XAUTH -v /tmp/.docker.xauth:/tmp/.docker.xauth:ro -v /tmp/.X11-unix:/tmp/.X11-unix:ro -v /dev/bus/usb:/dev/bus/usb $image_name /bin/bash
+docker run --net=host -i -t --entrypoint="bin/bash" --rm --privileged -e DISPLAY=$DISPLAY -e XAUTHORITY=$DOCKER_XAUTH -v /tmp/.docker.xauth:/tmp/.docker.xauth:ro -v /tmp/.X11-unix:/tmp/.X11-unix:ro -v /dev/bus/usb:/dev/bus/usb $image_name 
